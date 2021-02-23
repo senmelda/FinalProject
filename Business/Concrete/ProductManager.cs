ec8305e62ece4a9e8777569e539565c7073a232a
Melda Sen <senmelda13@gmail.com>
2021-02-23 23:38:47 +0300

    2302

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService//iş katmanının soyut sınıfı
+    {
+        IProductDal _productDal;//const
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //yetkisi var mı?
+            //bir takım kurallardan geçtikten sonra ürünleri sırala diyebiliriz.
+            //bir iş sınıfı diğer classları newlemez.
+            return _productDal.GetAll();
+
+        }
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -2,6 +2,7 @@
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -12,33 +13,57 @@ namespace DataAccess.Concrete.InMemory
         List<Product> _products;//global değişken
         public InMemoryProductDal()
         {
+            //Oracle,Sql Server , Postgres, MongoDb
             _products = new List<Product> {
                 new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitInStock=15},
-                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitInStock=15},
-                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitInStock=15},
-                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=15, UnitInStock=15},
-                new Product{ProductId=5, CategoryId=2, ProductName="Bardak", UnitPrice=15, UnitInStock=15}
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Bardak", UnitPrice=85, UnitInStock=1}
 
             };
         }
         public void Add(Product product)
         {
-            throw new NotImplementedException();
+            _products.Add(product);
         }
 
         public void Delete(Product product)
         {
-            throw new NotImplementedException();
+            //LINQ - Language Integrated Query
+            //Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);//foreach işlemi için linq yapısı. Genelde aramalarda singleordefault(iki tane veri gelirse hata gelir.) methodu kullanılır.
+
+
+            _products.Remove(productToDelete);//bu şekilde silinemez. Çünkü referans tipler referans numaraları üzerinden gider. Yukarıda tanımlananlar ile burada çağırılan ürünün referansı uyuşmaz.
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            return _products;
         }
 
-        public void Update(Product product)
+        public List<Product> GetAllByCategory(int categoryId)
         {
-            throw new NotImplementedException();
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)//ekrandan gelen data
+        {
+            //Gönderilen ürün id'sine sahip olan listedeki ürünü bul.
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);//güncellenecek referans bulunmalı.
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitInStock = product.UnitInStock;
         }
     }
 }
