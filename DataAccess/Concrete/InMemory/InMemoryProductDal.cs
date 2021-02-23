e85c3df78b332370c0c6a5f580528646fd4b07a0
Melda Sen <senmelda13@gmail.com>
2021-02-23 15:06:17 +0300

    last change

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+   
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;//global değişken
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitInStock=15},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitInStock=15},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=15, UnitInStock=15},
+                new Product{ProductId=5, CategoryId=2, ProductName="Bardak", UnitPrice=15, UnitInStock=15}
+
+            };
+        }
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
